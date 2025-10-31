50ad1b1069bb8084d69772a393b2d9050145cc43
Harsh Pandey <111627475+Harsh00004@users.noreply.github.com>
2025-10-31 14:43:25 +0530

    Update gitcode.tf

--- gitcode.tf
+++ gitcode.tf
@@ -24,7 +24,7 @@ resource "aws_vpc" "harsh_vpc" {
 resource "aws_subnet" "harsh_subnet" {
   vpc_id                  = aws_vpc.harsh_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-1"
+  availability_zone       = "us-west-1a"
   map_public_ip_on_launch = true
   tags = {
     Name = "HarshSubnet"
@@ -87,23 +87,6 @@ resource "aws_security_group" "harsh_sg" {
   }
 }
 
-resource "aws_instance" "harsh_ec2" {
-  ami                         = "ami-0c55b159cbfafe1f0"  # Valid in us-west-1
-  instance_type               = "t3.micro"
-                    = aws_subnet.harsh_subnet.id
-  vpc_security_group_ids      = [aws_security_group.harsh_sg.id]
-  associate_public_ip_address = true
-  key_name                    = "harsh-key"              # Ensure this key exists in us-west-1
-  tags = {
-    Name = "HarshEC2"
-  }
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.harsh_ec2.public_ip
-}
-
 output "s3_bucket_name" {
   value = aws_s3_bucket.harsh_bucket.bucket
 }
-
