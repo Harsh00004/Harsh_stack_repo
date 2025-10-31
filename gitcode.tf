ffdcf5f11ef84fb10e176390242038431445d257
Harsh Pandey <111627475+Harsh00004@users.noreply.github.com>
2025-10-31 14:22:33 +0530

    Update gitcode.tf

--- gitcode.tf
+++ gitcode.tf
@@ -1,109 +1,24 @@
-provider "aws" {
-  region = "us-west-1"
+ provider.tf                                                                               provider "aws" {
+  region = "ap-south-1"
 }
 
-resource "random_id" "rand" {
-  byte_length = 4
-}
-
-resource "aws_s3_bucket" "harsh_bucket" {
-  bucket = "harshbucket-${random_id.rand.hex}"
+# Create VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
-    Name        = "HarshBucket"
+    Name = "MyMainVPC"
+  }
+}
+
+# Create S3 Bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-terraform-s3-bucket-harsh5272"
+  acl    = "private"
+
+  tags = {
+    Name        = "MyS3Bucket"
     Environment = "Dev"
   }
-}
-
-resource "aws_vpc" "harsh_vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "HarshVPC"
-  }
-}
-
-resource "aws_subnet" "harsh_subnet" {
-  vpc_id                  = aws_vpc.harsh_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-1a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "HarshSubnet"
-  }
-}
-
-resource "aws_internet_gateway" "harsh_igw" {
-  vpc_id = aws_vpc.harsh_vpc.id
-  tags = {
-    Name = "HarshIGW"
-  }
-}
-
-resource "aws_route_table" "harsh_rt" {
-  vpc_id = aws_vpc.harsh_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.harsh_igw.id
-  }
-
-  tags = {
-    Name = "HarshRouteTable"
-  }
-}
-
-resource "aws_route_table_association" "harsh_rta" {
-  subnet_id      = aws_subnet.harsh_subnet.id
-  route_table_id = aws_route_table.harsh_rt.id
-}
-
-resource "aws_security_group" "harsh_sg" {
-  name        = "harsh-sg"
-  description = "Allow SSH and HTTP"
-  vpc_id      = aws_vpc.harsh_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "HarshSecurityGroup"
-  }
-}
-
-resource "aws_instance" "harsh_ec2" {
-  ami                         = "ami-0e6be795b21969e1d"
-  instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.harsh_subnet.id
-  vpc_security_group_ids      = [aws_security_group.harsh_sg.id]
-  associate_public_ip_address = true
-  key_name                    = "harsh-key"
-  tags = {
-    Name = "HarshEC2"
-  }
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.harsh_ec2.public_ip
-}
-
-output "s3_bucket_name" {
-  value = aws_s3_bucket.harsh_bucket.bucket
-
 }
