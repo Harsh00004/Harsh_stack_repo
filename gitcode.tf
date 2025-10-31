4da1d4fac0f006064f4bf56acc8c0ea62c42f528
Harsh Pandey <111627475+Harsh00004@users.noreply.github.com>
2025-10-31 15:37:00 +0530

    Update gitcode.tf

--- gitcode.tf
+++ gitcode.tf
@@ -1,5 +1,31 @@
+# -------------------------------
+# VARIABLES SECTION
+# -------------------------------
+variable "aws_region" {
+  default = "us-west-1"
+}
+
+variable "bucket_name_prefix" {
+  default = "harshbucket"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  default = "us-west-1a"
+}
+
+# -------------------------------
+# MAIN RESOURCES
+# -------------------------------
 provider "aws" {
-  region = "us-west-1"
+  region = var.aws_region
 }
 
 resource "random_id" "rand" {
@@ -7,7 +33,7 @@ resource "random_id" "rand" {
 }
 
 resource "aws_s3_bucket" "harsh_bucket" {
-  bucket = "harshbucket-${random_id.rand.hex}"
+  bucket = "${var.bucket_name_prefix}-${random_id.rand.hex}"
   tags = {
     Name        = "HarshBucket"
     Environment = "Dev"
@@ -15,7 +41,7 @@ resource "aws_s3_bucket" "harsh_bucket" {
 }
 
 resource "aws_vpc" "harsh_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
   tags = {
     Name = "HarshVPC"
   }
@@ -23,8 +49,8 @@ resource "aws_vpc" "harsh_vpc" {
 
 resource "aws_subnet" "harsh_subnet" {
   vpc_id                  = aws_vpc.harsh_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-1a"
+  cidr_block              = var.subnet_cidr
+  availability_zone       = var.availability_zone
   map_public_ip_on_launch = true
   tags = {
     Name = "HarshSubnet"
